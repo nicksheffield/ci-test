f80116819c5a743015a2ecf1c225c4a662baa8a2
Nick Sheffield <numbereft@gmail.com>
2024-04-17 11:27:15 +1200

    Update count display text in button onClick from "count is {count}" to "Number is {count}"

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
 			<h1>Vite + React</h1>
 			<div className="card">
 				<button onClick={() => setCount((count) => count + 4)}>
-					count is {count}
+					Number is {count}
 				</button>
 				<p>
 					Edit <code>src/App.tsx</code> and save to test HMR
