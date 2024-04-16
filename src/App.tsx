866706f55084a913760167d774765259362245e8
Nick Sheffield <numbereft@gmail.com>
2024-04-17 11:03:48 +1200

    Increase count increment in button onClick from 3 to 4

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
 			</div>
 			<h1>Vite + React</h1>
 			<div className="card">
-				<button onClick={() => setCount((count) => count + 3)}>
+				<button onClick={() => setCount((count) => count + 4)}>
 					count is {count}
 				</button>
 				<p>
